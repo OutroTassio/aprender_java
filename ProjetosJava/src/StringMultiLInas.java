b70cdedb59f89799ba4ed4b273062ac6ead84fb0
OutroTassio <tassio.samuel72@gmail.com>
2024-03-29 20:30:13 -0300

    aprendendo escrever strings em multiplas linhas

---
+++ ProjetosJava/src/StringMultiLInas.java
@@ -0,0 +1,10 @@
+public class StringMultiLInas {
+    public static void main(String[] args) {
+        String multiLinas = """
+            Um texto dividido 
+            em várias linas, mas
+            sem caracteres especiais.
+                """;
+        System.out.println(multiLinas);
+    }
+}
